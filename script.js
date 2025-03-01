976101751408d7f2347293db873a6e25d9e7e397
StylianosNicolaou <nicolaou.s24@live.unic.ac.cy>
2025-03-02 01:30:14 +0200

    removed "require"

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  require('dotenv').config();
-
   const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
